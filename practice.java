dd1d3564a951d1a86fc414c62e4d4d83d7fd2bd8
vickycode88 <vickypd2003@gmail.com>
2025-10-30 17:53:49 +0530

    New file added

---
+++ practice.java
@@ -0,0 +1,20 @@
+class Arith {
+    public static void main(String[] args) {
+
+        int add = 5 + 2;
+        System.out.println("Add:" + add);
+
+        int sub = 5 - 2;
+        System.out.println("sub:" + sub);
+
+        int mul = 5 * 2;
+        System.out.println("mul:" + mul);
+
+        int div = 5 + 2;
+        System.out.println("div:" + div);
+
+        int mod = 5 % 2;
+        System.out.println("Add:" + mod);
+
+    }
+}
