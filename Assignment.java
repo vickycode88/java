1733606059b42bb5670df5efc277c9be4c68df94
vickycode88 <vickypd2003@gmail.com>
2025-10-30 18:25:36 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,25 @@
+public class Assignment {
+
+    public static void main(String[] args) {
+
+        int x = 5;
+        System.out.println("Assignment: " + x);
+
+        x += 3;
+        System.out.println("Add: " + x);
+
+        x -= 2;
+        System.out.println("Sub: " + x);
+
+        x *= 4;
+        System.out.println("Mul: " + x);
+
+        x /= 2;
+        System.out.println("Division: " + x);
+
+        x %= 4;
+        System.out.println("Mod: " + x);
+
+    }
+
+}
