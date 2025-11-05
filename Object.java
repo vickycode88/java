37f281cc47af59521a0dd00708f60ba6c2617b72
vickycode88 <vickypd2003@gmail.com>
2025-11-05 11:07:59 +0530

    Update Object.java

--- Object.java
+++ Object.java
@@ -13,3 +13,29 @@ class InnerObject {
 
     
 }
+
+
+
+
+
+
+// class Model {
+//     String model="Ford";
+//     int year;
+
+//     void carInfo() {
+//         System.out.println("Model of car is :"+model+". Launch in year "+year);
+//     }
+// }
+
+// class Car {
+//     public static void main(String[] args) {
+//         Model m = new Model();
+
+//         // m.model = "Ferari";            // Intializating Value
+//         // m.year = 1934;                 // Intializating Value
+
+//         System.out.println("Model is :"+m.model);
+//         System.out.println("Launch Year is :"+m.year);
+//     }
+// }
