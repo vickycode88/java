ffed4b0689b929106065eae16eef232e5cdf56c6
vickycode88 <vickypd2003@gmail.com>
2025-11-05 14:43:05 +0530

    updated

--- InnerObject.java
+++ InnerObject.java
@@ -1,41 +1,32 @@
-class Object {
-    void mission(){
-        System.out.println("mission accomplished......");
+// class Object {
+//     void mission() {
+//         System.out.println("mission accomplished......");
+//     }
+// }
+
+// class InnerObject {
+//     public static void main(String[] args) {
+//         Object a = new Object();
+//         a.mission();
+//     }
+// }
+
+
+class A{
+int x=4;
+    static void  two(){
+        System.out.println("hello");
+
     }
 }
 
-class InnerObject {
-    public static void main(String[] args) {
-         Object a = new Object();
-         a.mission();
-    }
-   
+class B {
 
+    public static void main(String[] args) {
+        A n = new A();
+        System.out.println(n.x);
+        A.two();
+
+    }
     
 }
-
-
-
-
-
-
-// class Model {
-//     String model="Ford";
-//     int year;
-
-//     void carInfo() {
-//         System.out.println("Model of car is :"+model+". Launch in year "+year);
-//     }
-// }
-
-// class Car {
-//     public static void main(String[] args) {
-//         Model m = new Model();
-
-//         // m.model = "Ferari";            // Intializating Value
-//         // m.year = 1934;                 // Intializating Value
-
-//         System.out.println("Model is :"+m.model);
-//         System.out.println("Launch Year is :"+m.year);
-//     }
-// }
