65eb4bf42c04acfee211cd95eceaee1ed7a5c563
vickycode88 <vickypd2003@gmail.com>
2025-11-10 18:33:42 +0530

    Create Constructor_example.java

---
+++ Constructor_example.java
@@ -0,0 +1,27 @@
+
+class Calculator {
+    
+    Calculator(int a, int b) {
+        int sum = a + b;
+        int diff = a - b;
+        int product = a * b;
+        int div = a/b;
+        int area = (a * b) / 2;  
+
+        
+        System.out.println("Addition: " + sum);
+        System.out.println("Subtraction: " + diff);
+        System.out.println("Multiplication: " + product);
+        System.out.println("dive : "+ div);
+        System.out.println("Area of Triangle: " + area);
+    }
+
+      public static void main(String[] args) {
+        
+        Calculator calc = new Calculator(10, 5);
+    }
+}
+
+
+  
+
