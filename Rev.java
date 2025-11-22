9e2621383625b7636dfc4523b9bc4de9cbe83ae4
vickycode88 <vickypd2003@gmail.com>
2025-11-22 16:06:27 +0530

    Create Rev.java

---
+++ Rev.java
@@ -0,0 +1,22 @@
+public class Rev {
+
+    public static void main(String[] args) {
+        
+       int  n= 1234;
+
+       int rev = 0;
+
+       while (n>0) {
+
+        int d= n%10;
+        rev = rev*10+d;
+        n=n/10;
+
+        
+       }
+
+       System.out.println("reverse of "+n+" is :"+rev);
+
+    }
+    
+}
