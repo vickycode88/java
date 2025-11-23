5d7e4d43c2b09f090846bcc3522404ed8d7274f2
vickycode88 <vickypd2003@gmail.com>
2025-11-23 21:13:24 +0530

    Create Method_overriding.java

---
+++ Method_overriding.java
@@ -0,0 +1,35 @@
+class Avenger {
+    void attack() {
+        System.out.println("Avenger attacks!");
+    }
+}
+
+class IronMan extends Avenger {
+    void attack() { System.out.println("Iron Man attacks with lasers!"); }
+}
+
+class Thor extends Avenger {
+    void attack() { System.out.println("Thor attacks with lightning!"); }
+}
+
+class Hulk extends Avenger {
+    void attack() { System.out.println("Hulk smashes everything!"); }
+}
+
+
+class MethodOveriding {
+    
+    public static void main(String[] args) {
+        Avenger a;
+
+        a = new IronMan(); 
+        a.attack();  
+
+        a = new Thor();
+        a.attack(); 
+
+        a = new Hulk();    
+        a.attack();  
+
+    }
+}
