b75a71516576332784ccb486edb2168d80a6f5f4
vickycode88 <vickypd2003@gmail.com>
2025-11-24 12:34:48 +0530

    updated one d array

---
+++ OneD_array_input.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class ArrayInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt(); 
+
+        int[] arr = new int[size];  
+
+        System.out.println("Enter " + size + " elements:");
+
+        // Input values using a loop
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();  
+        }
+
+        System.out.println("The array elements are:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();  
+    }
+}
