a10484bd57b7a9ed2b909fb9fb3d170a8385d436
vickycode88 <vickypd2003@gmail.com>
2025-11-25 20:21:08 +0530

    Update Concept_2.java

--- Inter/Concept_2.java
+++ Inter/Concept_2.java
@@ -2,18 +2,18 @@ package Inter;
 
 interface A {
     int x=20;
-    void a1();
+    void m1();
 }
 
 interface B extends A {
     int x=30;
-    void a2();
+    void m2();
 }
 
 //  If we Don't implements both the method (m1, m2) in sub class then we have to make Demo class as abstract class.
 class Demo implements B {
     
-    public void a1() {
+    public void m1() {
         System.out.println("m1 implemented");
         System.out.println("value of x from Interface A: "+A.x);   // InterfaceName.constant
         System.out.println("value of x from Interface B: "+B.x);   // InterfaceName.constant
@@ -21,15 +21,16 @@ class Demo implements B {
         // If we don't use InterfaceName.constant then result will be 30 for both
     }
     
-    public void a2() {
+    public void m2() {
         System.out.println("m2 implemented");
     }
 }
 
+
 public class Concept_2 {
     public static void main(String[] args) {
         Demo obj = new Demo();
-        obj.a1();
-        obj.a2();
+        obj.m1();
+        obj.m2();
     }
 }
